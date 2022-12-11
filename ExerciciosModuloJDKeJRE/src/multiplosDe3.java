1d11c946a48a6a4006268eece6561cb59c380904
wesllanSilva <wesllan90@gmail.com>
2022-12-11 19:37:45 -0300

    adicionado pasta com exercicios do primeiro modulo - exer01 multiplos de 3

---
+++ ExerciciosModuloJDKeJRE/src/multiplosDe3.java
@@ -0,0 +1,11 @@
+
+
+public class multiplosDe3 {
+	public static void main(String[] args) {
+		for (int contador = 0; contador <= 100; contador++) {
+			if (contador % 3 == 0) {
+				System.out.println(contador + " É multiplo de 3");
+			}
+		}
+	}
+}
